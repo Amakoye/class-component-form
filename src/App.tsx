99c9006ab38b3189a4fe731b5bee6093129da91c
Amakoye <charlesamakoye@gmail.com>
2022-11-21 11:31:26 +0300

    working with forms and state

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Form from "./Form";
 
 class App extends React.Component {
   render(): React.ReactNode {
+    console.log("Render app");
+
     return (
       <div className="App">
         <header className="App-header">
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,7 +1,56 @@
 import React from "react";
 
-export default class Form extends React.Component {
+interface FormState {
+  fname: string;
+  mname: string;
+  lname: string;
+}
+
+export default class Form extends React.Component<any, FormState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      mname: "",
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ ...this.state, [e.target.name]: e.target.value });
+  }
+
+  onSubmit() {
+    const { fname, lname, mname } = this.state;
+    if (fname && lname && mname) {
+      alert(`You entered the following details \n ${fname} ${mname} ${lname}`);
+    }
+  }
+
+  componentDidMount(): void {
+    console.log("Form component mounted");
+  }
+
   render() {
-    return <>hello form</>;
+    console.log("render form");
+
+    return (
+      <>
+        <input type={"text"} name="fname" onChange={(e) => this.onChange(e)} />
+        <input type={"text"} name="mname" onChange={(e) => this.onChange(e)} />
+        <input type={"text"} name="lname" onChange={(e) => this.onChange(e)} />
+        <input type="submit" value="submit" onClick={() => this.onSubmit()} />
+      </>
+    );
+  }
+
+  componentDidUpdate(
+    prevProps: Readonly<any>,
+    prevState: Readonly<FormState>,
+    snapshot?: any
+  ): void {
+    console.log("Form component did update");
   }
 }
